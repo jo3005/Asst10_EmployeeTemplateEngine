ebf173e87b342c0a76024950f4398e5318888449
jo3005 <jo@caswa.com>
2020-04-29 11:15:39 +0800

    Recording the team information from the user input using recursion.  This bit works!!!!

--- app.js
+++ app.js
@@ -3,33 +3,149 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+//const fs = require("fs");
+let data_results=[];
+let more_employees='Hallibut';
+
+
+
+const data_results_temp=
+  [
+    Manager {
+      name: 'Fred Boss',
+      id: 'fred@email.com',
+      email: '51',
+      role: 'Manager',
+      officeNumber: '0892770900'
+    },
+    Engineer {
+      name: 'Joe Engine',
+      id: '36',
+      email: 'joe@email.com',
+      role: 'Engineer',
+      github: 'jotheman'
+    },
+    Intern {
+      name: 'Coffee Boy',
+      id: '9008',
+      email: 'coffee@email.com',
+      role: 'Intern',
+      school: 'Barista High'
+    },
+    Engineer {
+      name: 'Jane theWiz',
+      id: '86',
+      email: 'jane@email.com',
+      role: 'Engineer',
+      github: 'janeWiz'
+    }
+  ]
+
+
+console.log("Please build your team.");
+
+function getPersonData(){
+    const doMore="No";
+    more_employees="Hallibut";
+    inquirer.prompt([
+        {   type: "input",
+            message: "Enter the person's name:",
+            name: "name",
+            default: "Fred Boss"
+        },
+        {   type: "input",
+            message: "Enter their employee id:",
+            name: "id",
+            default: "51"
+        },
+        {   type: "input",
+            message: "Enter their email address:",
+            name: "email",
+            default: "fred@email.com"
+        },
+        {
+            type: "list",
+            message: "What type of role does the person have?",
+            name: "role",
+            choices:["Manager",
+                    "Engineer",
+                    "Intern"],
+            default: "Engineer"
+        },
+        {
+            type: "input",
+            message: "Enter the Manager's office number:",
+            name: "officeNumber",
+            when: function(answers){
+                return(answers.role==="Manager")
+            }
+        },
+        {
+            type: "input",
+            message: "Enter their github account id:",
+            name: "github",
+            when: function(answers){
+                return(answers.role==="Engineer")
+            }
+        },
+        {
+            type: "input",
+            message: "What school do they attend?",
+            name: "school",
+            when: function(answers){
+                return(answers.role==="Intern")
+            }
+        },
+        {
+            type: "list",
+            message: "Do you want to enter another employee?",
+            name: "more",
+            choices: ["Yes", "No"],
+            default: "Yes"
+        }
+    ])
+    .then(data => {
+        
+        data_results.push(data);
+        if (data.more !=='Yes') { 
+            return data_results;
+        } else {
+            getPersonData();
+        }
+        
+    })
+    .then(response => {
+        if(response!== undefined) {
+            //console.log(response);
+            const persons=[];
+            response.forEach(person => {
+                switch(person.role){
+                    case "Manager":
+                        const newManager= new Manager(person.name, person.email, person.id,person.officeNumber);
+                        persons.push(newManager);
+                        break;
+                    case "Engineer":
+                        const newEngineer = new Engineer(person.name, person.email,person.id, person.github);
+                        persons.push(newEngineer);
+                        break;
+                    case "Intern":
+                        const newIntern = new Intern(person.name, person.id, person.email, person.school);
+                        persons.push(newIntern);
+                        break;
+                    default:
+                        const newEmployee= new Employee(person.name,person.id, person.email);
+                        persons.push(newEmployee);
+                        break;
+                    };
+                    
+            })
+            //console.log(persons);
+            return persons;
+        }  
+    })
+    .then(team => {
+
+    });   
+}
+getPersonData();
+
