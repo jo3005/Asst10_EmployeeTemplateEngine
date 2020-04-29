f9d8705df4d449d3b1944eb983237c814c2a158a
jo3005 <jo@caswa.com>
2020-04-29 12:29:24 +0800

    Rendering the data to the  html file.   Inquiry section currently commented out.

--- app.js
+++ app.js
@@ -3,44 +3,47 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-//const fs = require("fs");
-let data_results=[];
+const fs = require("fs");
+
+
+const OUTPUT_DIR = path.resolve(__dirname,"output");
+const outputPath = path.join(OUTPUT_DIR,"team.html")
+const render = require("./lib/htmlRenderer");
+
+
 let more_employees='Hallibut';
-
-
-
-const data_results_temp=
+let team=[];
+let team_default=
   [
-    Manager {
-      name: 'Fred Boss',
-      id: 'fred@email.com',
-      email: '51',
-      role: 'Manager',
-      officeNumber: '0892770900'
-    },
-    Engineer {
-      name: 'Joe Engine',
-      id: '36',
-      email: 'joe@email.com',
-      role: 'Engineer',
-      github: 'jotheman'
-    },
-    Intern {
-      name: 'Coffee Boy',
-      id: '9008',
-      email: 'coffee@email.com',
-      role: 'Intern',
-      school: 'Barista High'
-    },
-    Engineer {
-      name: 'Jane theWiz',
-      id: '86',
-      email: 'jane@email.com',
-      role: 'Engineer',
-      github: 'janeWiz'
-    }
-  ]
-
+    new Manager(
+      name= 'Fred Boss',
+      id= 'fred@email.com',
+      email= '51',
+      role= 'Manager',
+      officeNumber= '0892770900'
+    ),
+   new Engineer (
+      name= 'Joe Engine',
+      id= '36',
+      email= 'joe@email.com',
+      role= 'Engineer',
+      github= 'jotheman'
+    ),
+    new Intern (
+      name= 'Coffee Boy',
+      id= '9008',
+      email= 'coffee@email.com',
+      role= 'Intern',
+      school= 'Barista High'
+    ),
+    new Engineer (
+      name= 'Jane theWiz',
+      id= '86',
+      email= 'jane@email.com',
+      role= 'Engineer',
+      github= 'janeWiz'
+    )
+  ];
 
 console.log("Please build your team.");
 
@@ -106,46 +109,55 @@ function getPersonData(){
     ])
     .then(data => {
         
-        data_results.push(data);
+        team.push(data);
         if (data.more !=='Yes') { 
-            return data_results;
+            return team;
         } else {
             getPersonData();
         }
         
     })
     .then(response => {
-        if(response!== undefined) {
-            //console.log(response);
-            const persons=[];
-            response.forEach(person => {
-                switch(person.role){
-                    case "Manager":
-                        const newManager= new Manager(person.name, person.email, person.id,person.officeNumber);
-                        persons.push(newManager);
-                        break;
-                    case "Engineer":
-                        const newEngineer = new Engineer(person.name, person.email,person.id, person.github);
-                        persons.push(newEngineer);
-                        break;
-                    case "Intern":
-                        const newIntern = new Intern(person.name, person.id, person.email, person.school);
-                        persons.push(newIntern);
-                        break;
-                    default:
-                        const newEmployee= new Employee(person.name,person.id, person.email);
-                        persons.push(newEmployee);
-                        break;
-                    };
-                    
-            })
-            //console.log(persons);
-            return persons;
-        }  
+        if(response === undefined) {
+            return;
+        }
+        //console.log(response);
+        const persons=[];
+        response.forEach(person => {
+            switch(person.role){
+                case "Manager":
+                    const newManager= new Manager(person.name, person.email, person.id,person.officeNumber);
+                    persons.push(newManager);
+                    break;
+                case "Engineer":
+                    const newEngineer = new Engineer(person.name, person.email,person.id, person.github);
+                    persons.push(newEngineer);
+                    break;
+                case "Intern":
+                    const newIntern = new Intern(person.name, person.id, person.email, person.school);
+                    persons.push(newIntern);
+                    break;
+                default:
+                    const newEmployee= new Employee(person.name,person.id, person.email);
+                    persons.push(newEmployee);
+                    break;
+                };
+                
+        })
+        //console.log(persons);
+        return persons; 
     })
-    .then(team => {
-
+    .then(team_members => {
+        
+        if(team_members!==undefined){
+            return;
+        }
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+          }
+        fs.writeFileSync(outputPath, render(team), "utf-8");
     });   
 }
-getPersonData();
+//getPersonData();
+fs.writeFileSync(outputPath, render(team_default), "utf-8");
 
